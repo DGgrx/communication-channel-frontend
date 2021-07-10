f07c6a28e7889e9e2f8afbca016f33de50efd6ad
DGgrx <divyanshgarg.divs@gmail.com>
2021-07-10 10:46:32 +0530

    deploy

--- src/useConfig.js
+++ src/useConfig.js
@@ -1,5 +1,8 @@
 // export const api = "https://communication-channel-backend.herokuapp.com/api/";
 // export const socket_api = "https://communication-channel-backend.herokuapp.com/"
 
-export const api = "http://18.207.115.53:3000/api/";
-export const socket_api = "http://18.207.115.53:3000/"
+// export const api = "http://18.207.115.53:3000/api/";
+// export const socket_api = "http://18.207.115.53:3000/"
+
+export const api = "https://zine-chat-back.herokuapp.com/api/";
+export const socket_api = "https://zine-chat-back.herokuapp.com"
